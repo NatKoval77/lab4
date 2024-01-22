b102b56e78d4a655ac7d436eb2487e319425ba21
NatKoval77 <agimol@mail.ru>
2024-01-23 00:20:18 +0300

    Refresh helloword.cpp

--- helloword.cpp
+++ helloword.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 
-using namespace std;
-
-int main(){
-  cout << "Hello World!";
-  return 0;
+int main()
+{
+string name;
+std::cout<<"What is your name?";
+std::cin>>name;
+std::cout<<"Hello world" << name << endl;
+return 0;
 }
